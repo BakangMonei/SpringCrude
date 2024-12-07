908d33c122d5ff1dc0e443c599920e4306fb8238
BakangMonei <bakangmonei2@gmail.com>
2024-12-07 20:00:00 +0200

    Adding services

--- src/main/java/com/moneibakang/SpringCrude/Service/UserService.java
+++ src/main/java/com/moneibakang/SpringCrude/Service/UserService.java
@@ -1,4 +1,32 @@
 package com.moneibakang.SpringCrude.Service;
 
+import com.moneibakang.SpringCrude.Model.User;
+import com.moneibakang.SpringCrude.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findById(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String email) {
+        userRepository.deleteById(email);
+    }
 }
