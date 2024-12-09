3ce4b349e89adc8ed65cc5806a65a3709fd16cbc
BakangMonei <bakangmonei2@gmail.com>
2024-12-09 20:00:00 +0200

    Adding controllers code and endpoints

--- src/main/java/com/moneibakang/SpringCrude/Controller/UserController.java
+++ src/main/java/com/moneibakang/SpringCrude/Controller/UserController.java
@@ -1,4 +1,60 @@
 package com.moneibakang.SpringCrude.Controller;
 
+import com.moneibakang.SpringCrude.Model.User;
+import com.moneibakang.SpringCrude.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*") // Allow connections from React Frontend
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        Optional<User> user = userService.getUserByEmail(email);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{email}")
+    public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User userDetails) {
+        Optional<User> userOptional = userService.getUserByEmail(email);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setFirstname(userDetails.getFirstname());
+            user.setLastname(userDetails.getLastname());
+            user.setPhonenumber(userDetails.getPhonenumber());
+            user.setPhysicaladdress(userDetails.getPhysicaladdress());
+            return ResponseEntity.ok(userService.saveUser(user));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String email) {
+        if (userService.getUserByEmail(email).isPresent()) {
+            userService.deleteUser(email);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
